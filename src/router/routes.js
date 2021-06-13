bf8eef9d251aabbfe001c7e7dbdd779e401d16f4
Gabriel Miranda <grmiranda97@gmail.com>
2021-06-13 15:24:55 -0300

    Clean project and update font

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,18 +1,12 @@
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: () => import('@/pages/Home.vue'),
-  },
-  {
-    path: '/about',
-    name: 'about',
     component: () => import('@/components/layouts/BlankLayout.vue'),
     children: [
       {
         path: '',
-        name: 'about-1',
-        component: () => import('@/pages/About.vue'),
+        name: 'InvoiceGenerator',
+        component: () => import('@/pages/InvoiceGenerator.vue'),
       },
     ],
   },
